4645d9a684b7a63a6361219d5c3e856b2d6e2bdf
Shun Sato <shunsato19951231@gmail.com>
2020-08-06 15:30:25 +0900

    add validation InputComponent

--- src/Components/atoms/Form/Form.component.tsx
+++ src/Components/atoms/Form/Form.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Form.style.styl'
 
 interface InputProps extends React.Props<{}> {
@@ -6,6 +6,7 @@ interface InputProps extends React.Props<{}> {
     theme?: 'initialSetting',
     placeholder?: string
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    errorMessage?: string,
     value: string, 
 }
 
@@ -14,12 +15,40 @@ export const Input:React.FC<InputProps> = ({
     theme,
     placeholder,
     onChange,
+    errorMessage,
     value,
-}): JSX.Element => (
-    <input
-      className={`${styles[theme]} ${className || ''}`}
-      onChange={onChange}
-      placeholder={placeholder}
-      value={value}
-    />
-)
+}): JSX.Element => {
+  const [initial, setInitial] = useState<boolean>(true)
+  const didMountRef = React.useRef(false)
+
+  useEffect(() => {
+    if(didMountRef.current) {
+      setInitial(false)
+    } else {
+      didMountRef.current = true
+    }
+  }, [value])
+
+  return (
+    <div className={`${className || ''}`}>
+      <input
+        data-testid='input'
+        className={`${styles[theme]} ${
+          initial || errorMessage === undefined ? ''
+          : errorMessage ? styles.validateError
+          : styles.validateOk
+          
+        }`}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+      />
+      { errorMessage !== undefined &&
+        <p
+          data-testid='errorMessage'
+          className={styles.errorMessage}
+        >{initial ? '' : errorMessage}</p>
+      }
+    </div>
+  )
+}
--- src/Components/atoms/Form/Form.test.tsx
+++ src/Components/atoms/Form/Form.test.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Input } from './Form.component'
+import { shallow, mount } from 'enzyme'
 
+import styles from './Form.style.styl'
+
+const sel = (id: string) => {
+  return `[data-testid="${id}"]`
+}
 
 describe('[ATOMS] Form', () => {
 
@@ -20,5 +26,95 @@ describe('[ATOMS] Form', () => {
       className='testClass'
     />, div)
     ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should hidden error message and not set style when initial loading and errorMessage is string', () => {
+    let errorMessage: string = 'error!'
+    const wrapper = shallow(<Input
+      value=''
+      errorMessage={errorMessage}
+    />)
+
+    const input = wrapper.find(sel('input'))
+    const text = wrapper.find(sel('errorMessage'))
+
+    expect(input.hasClass(styles.validateError)).toBe(false)
+    expect(input.hasClass(styles.validateOk)).toBe(false)
+    expect(text.text()).toEqual('')
+  })
+
+  it('should set error style when errorMessage is string and changed message', () => {
+    let errorMessage: string = 'error!'
+    const wrapper = mount(<Input
+      theme='initialSetting'
+      value=''
+      errorMessage={errorMessage}
+    />)
+
+    //change text
+    wrapper.setProps({value: 'test'})
+    wrapper.update()
+
+    const input = wrapper.find(sel('input'))
+    const text = wrapper.find(sel('errorMessage'))
+
+    expect(input.hasClass(styles.validateError)).toBe(true)
+    expect(input.hasClass(styles.validateOk)).toBe(false)
+    expect(text.text()).toEqual(errorMessage)
+  })
+
+  it('should hidden error message and not set style when initial loading and errorMessage is null', () => {
+    let errorMessage: string = null
+    const wrapper = shallow(<Input
+      value=''
+      errorMessage={errorMessage}
+    />)
+
+    const input = wrapper.find(sel('input'))
+    const text = wrapper.find(sel('errorMessage'))
+
+    expect(input.hasClass(styles.validateError)).toBe(false)
+    expect(input.hasClass(styles.validateOk)).toBe(false)
+    expect(text.text()).toEqual('')
+  })
+
+  it('should set error style when errorMessage is null and changed message', () => {
+    let errorMessage: string = null
+    const wrapper = mount(<Input
+      value=''
+      errorMessage={errorMessage}
+    />)
+
+    //change text
+    wrapper.setProps({value: 'test'})
+    wrapper.update()
+
+    const input = wrapper.find(sel('input'))
+    const text = wrapper.find(sel('errorMessage'))
+
+    expect(input.hasClass(styles.validateError)).toBe(false)
+    expect(input.hasClass(styles.validateOk)).toBe(true)
+    expect(text.text()).toEqual('')
+  })
+
+
+  it('should hide errorStyle and error message when errorMessage props is undefined', () => {
+    const wrapper = mount(<Input
+      value=''
+    />)
+
+    const input = wrapper.find(sel('input'))
+    const text = wrapper.find(sel('errorMessage'))
+
+    expect(text.length).toBe(0)
+
+    //change text
+    jest.spyOn(React, 'useRef').mockReturnValueOnce({ current: true })
+    input.props().value = 'test'
+    input.simulate('change')
+
+    expect(input.hasClass(styles.validateError)).toBe(false)
+    expect(input.hasClass(styles.validateOk)).toBe(false)
+    expect(text.length).toBe(0)
   })
 })
