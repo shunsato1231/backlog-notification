1687a64a14d417817a2fee2b96b2b2dc2266a232
Shun Sato <shunsato19951231@gmail.com>
2020-09-01 13:29:06 +0900

    add LocalStorageHooks generics type

--- src/Hooks/LocalStorage/LocalStorage.hook.tsx
+++ src/Hooks/LocalStorage/LocalStorage.hook.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
-export const useLocalStorage = (key: string, initialValue: any): [any, (value: any) => Promise<void>] => {
-  const [storedValue, setStoredValue] = useState(() => {
+export const useLocalStorage = <T extends any>(key: string, initialValue: T): [T, (value: T) => Promise<void>] => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = window.localStorage.getItem(key)
       return item ? JSON.parse(item) : initialValue
@@ -10,7 +10,7 @@ export const useLocalStorage = (key: string, initialValue: any): [any, (value: a
     }
   })
 
-  const setValue = async (value: any) => {
+  const setValue = async (value: T) => {
     try {
       const valueToStore = value instanceof Function ? value(storedValue) : value
       setStoredValue(valueToStore)
